57e6cd637edf8abfd6ea155c16dbfbc014a5e701
Matthew Precious <mattprecious@gmail.com>
2023-08-21 23:37:32 -0400

    Add minimal styling to the list (#19)

--- app/src/main/kotlin/com/jakewharton/sa4p/ui/pendingUrls.kt
+++ app/src/main/kotlin/com/jakewharton/sa4p/ui/pendingUrls.kt
@@ -1,20 +1,78 @@
 package com.jakewharton.sa4p.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.jakewharton.sa4p.presenter.MainModel
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.jakewharton.sa4p.db.Pending
+import com.jakewharton.sa4p.db.Urls
+import kotlinx.datetime.Instant
 
 @Composable
-fun PendingUrls(model: MainModel) {
-	if (model.pendingUrls.isEmpty()) {
-		Text("No URLs to sync!")
+fun PendingUrls(urls: List<Pending>) {
+	if (urls.isEmpty()) {
+		ListItem(headlineContent = { Text("No URLs to sync!") })
 	} else {
 		LazyColumn {
-			items(model.pendingUrls) { pending ->
-				Text(pending.url)
+			items(urls) { pending ->
+				Column {
+					Divider()
+					ListItem(
+						headlineContent = {
+							Text(
+								text = pending.url,
+								maxLines = 1,
+								overflow = TextOverflow.Ellipsis,
+							)
+						},
+					)
+				}
 			}
 		}
 	}
+}
+
+@Preview
+@Composable
+fun PendingUrlsEmptyPreview() {
+	PendingUrls(urls = emptyList())
+}
+
+@Preview
+@Composable
+fun PendingUrlsPopulatedPreview() {
+	PendingUrls(
+		urls = listOf(
+			Pending(
+				id = Urls.Id(0),
+				url = "https://example.com/ball.php",
+				added = Instant.parse("2017-06-11T00:00:00.000Z"),
+			),
+			Pending(
+				id = Urls.Id(0),
+				url = "https://www.example.com/bell/army.html",
+				added = Instant.parse("2017-06-11T00:00:00.000Z"),
+			),
+			Pending(
+				id = Urls.Id(0),
+				url = "https://www.example.com/",
+				added = Instant.parse("2017-06-11T00:00:00.000Z"),
+			),
+			Pending(
+				id = Urls.Id(0),
+				url = "https://activity.example.com/airplane.html?back=bag",
+				added = Instant.parse("2017-06-11T00:00:00.000Z"),
+			),
+			Pending(
+				id = Urls.Id(0),
+				url = "https://attraction.example.com/box/bat",
+				added = Instant.parse("2017-06-11T00:00:00.000Z"),
+			),
+		),
+	)
 }
